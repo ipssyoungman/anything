712a42ff1b8e4cc263789b96ce2eca9604719ebd
ipssyoungman <36124832+ipssyoungman@users.noreply.github.com>
2018-02-04 17:16:53 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,29 +3,20 @@ from functools import reduce
 def str2float(s):
     def intfn(x,y):
         return x*10+y
-    def flofn(x,y):
-        return x*10**-1 + y
-    for i in range(len(s)):
-      num=s.find('.')                     #小数点的位置
-        while i<num:
-            int_part=reduce(intfn,map(int,s[i]))
-        if s[i]!='.':
-            reduce(flofn,map(int,s[i]))
-            else:
-                pass
-
-print('str2float(\'123\') =', str2float('123'))
-if abs(str2float('123') - 123) < 0.00001:
-    print('测试成功!')
-else:
-    print('测试失败!')
+    num=s.find('.')
+    for i in range(num):
+        int_part=reduce(intfn,map(int,s[i]))
+    for j in range(num+1,len(s)):
+        flo_part=reduce(intfn,map(int,s[i])
+    return int_part+flo_part*10**(-(len(s)-num-1))
+            
 
 
 
-'''
+
+
 print('str2float(\'123.456\') =', str2float('123.456'))
 if abs(str2float('123.456') - 123.456) < 0.00001:
     print('测试成功!')
 else:
     print('测试失败!')
-'''
